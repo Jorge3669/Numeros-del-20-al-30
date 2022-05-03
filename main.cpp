01029f3d3b07a173134a72527c67a4f1922e18f7
Jorge3669 <101146927+Jorge3669@users.noreply.github.com>
2022-05-02 21:56:43 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/*CONSTRUIR UN PROGRAMA QUE DESPLIEGUE LOS NÚMEROS DEL 20 AL 30.*/
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+	for(int i=20;i<=30;i++){
+		cout<<i<<endl;
+	}
+	system("pause");
+}
